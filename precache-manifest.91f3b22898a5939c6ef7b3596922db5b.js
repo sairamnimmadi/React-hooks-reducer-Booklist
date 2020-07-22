3106ef5e38e4042e158cbcdd6d3c8b89e86431ea
sairam <sairamnimmadi@gmail.com>
2020-07-22 15:50:16 +0530

    Updates

---
+++ precache-manifest.91f3b22898a5939c6ef7b3596922db5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df111536a0cbbcbeb31f87dc0ac16ddf",
+    "url": "/React-hooks-reducer-Booklist/index.html"
+  },
+  {
+    "revision": "597db4de34fa40c7f9ba",
+    "url": "/React-hooks-reducer-Booklist/static/css/main.e1d680f3.chunk.css"
+  },
+  {
+    "revision": "35481f33429e764a0f4a",
+    "url": "/React-hooks-reducer-Booklist/static/js/2.75dd7b12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-hooks-reducer-Booklist/static/js/2.75dd7b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597db4de34fa40c7f9ba",
+    "url": "/React-hooks-reducer-Booklist/static/js/main.e593f025.chunk.js"
+  },
+  {
+    "revision": "9b5fdb10aaf4230eaaf3",
+    "url": "/React-hooks-reducer-Booklist/static/js/runtime-main.e3460aeb.js"
+  }
+]);
